6f75be02e40f02162bc11b531b2e82a8b8c5b126
Jinakali <mutuaericmurithi@gmail.com>
2023-10-15 06:15:06 +0300

    fix: loop when % detected

---
+++ test/test1/main2.c
@@ -0,0 +1,95 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    int len = 0;
+    int len2 = 0;
+    int ZERO = 0;
+
+    len = printf("String:[%s%s]\n", "I am a string !", "second string !");
+    len2 = _printf("String:[%s%s]\n", "I am a string !", "second string !");
+    printf("Length:[%d, %i]\n", len, len2);
+
+    len = _printf("Number: %d, %i\n", 4034, 4034);
+    len2 = printf("Number: %d, %i\n", 4034, 4034);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("Negative: %i, %d\n", -4034, -4034);
+    len2 = printf("Negative: %i, %d\n", -4034, -4034);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("NULL: %i\n", NULL);
+    len2 = printf("NULL: %i\nlenlen", NULL);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("%d\n", INT_MAX);
+    len2 = printf("%d\n", INT_MAX);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("%d\n", INT_MIN);
+    len2 = printf("%d\n", INT_MIN);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("\nNumber: [%d], [%i]\n", 4034, 4034);
+    len2 = printf("Number: [%d], [%i]\n", 4034, 4034);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("Negative: [%i], [%d]\n", -4034, -4034);
+    len2 = printf("Negative: [%i], [%d]\n", -4034, -4034);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("long: [%i], [%d]\n", 4545439394949595, 4545439394949595);
+    len2 = printf("long: [%i], [%d]\n", 4545439394949595, 4545439394949595);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("Greater than int: [%i], [%d]\n", 454543939494, 454543939494);
+    len2 = printf("Greater than int: [%i], [%d]\n", 454543939494, 454543939494);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("N-Greater than int: [%i], [%d]\n", -454543939494, -454543939494);
+    len2 = printf("N-Greater than int: [%i], [%d]\n", -454543939494, -454543939494);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("long: [%i], [%d]\n", -4545439394949595, -4545439394949595);
+    len2 = printf("long: [%i], [%d]\n", -4545439394949595, -4545439394949595);
+    printf("Len:[%d]\n", len);
+
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("NULL: [%i]\n", NULL);
+    len2 = printf("NULL: [%i]\n", NULL);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("Zero: [%i]\n",ZERO);
+    len2 = printf("Zero: [%i]\n",ZERO);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("[%d]\n", INT_MAX);
+    len2 = printf("[%d]\n", INT_MAX);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    len = _printf("[%d]\n", INT_MIN);
+    len2 = printf("[%d]\n", INT_MIN);
+    printf("Len:[%d]\n", len);
+    printf("Lelen:[%d]\n\n", len2);
+
+    _printf(NULL);
+    printf(NULL);
+
+    return (0);
+}
--- test/test1/printf.c
+++ test/test1/printf.c
@@ -1,42 +1,43 @@
 #include "main.h"
 /**
-  *_printf-produces output according to a format
-  *@format:character string
-  *Return:number of characters printed except null byte
-  */
+ *_printf-produces output according to a format
+ *@format:character string
+ *Return:number of characters printed except null byte
+ */
 int _printf(const char *format, ...)
 {
 	int i, j, sum = 0, num1_chars = 0, num2_chars = 0;
 	va_list par;
 
-	/*array of struct with format characters and associated functions*/
 	fmt conversion[] = {
-	{'c', print_c},
-	{'s', print_s},
-	{'%', print_p},
-	{'i', print_i_d},
-	{'d', print_i_d},
-	{'\0', NULL}
+		{'c', print_c},
+		{'s', print_s},
+		{'%', print_p},
+		{'i', print_i_d},
+		{'d', print_i_d},
+		{'\0', NULL}
 	};
-
 	va_start(par, format);
-
 	for (i = 0; format != NULL && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
+		{
+			num2_chars = write(2, &format[i], 1);
+			sum += num2_chars;
+		}
+		while (format[i] == '%')
 		{
 			for (j = 0; conversion[j].f != '\0'; j++)
 			{
 				if (conversion[j].f == format[i + 1])
 				{
 					num1_chars = conversion[j].func(par);
-					i = i + 2;/*skip to next char after specifier*/
+					i = i + 1;/*skip format apecifier*/
 					sum += num1_chars;
+					break;
 				}
 			}
 		}
-		num2_chars = write(2, &format[i], 1);
-		sum += num2_chars;
 	}
 	va_end(par);
 	return (sum);
